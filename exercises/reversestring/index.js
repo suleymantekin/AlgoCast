d1d0eeb6c94831463a3ecba24e210ffd81da4654
suleymantekin <suleymantekin@users.noreply.github.com>
2018-09-15 18:37:41 +0300

    Loop over the string and unshift each char to the new string

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,19 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+// Use array reverse method
+// function reverse(str) {
+//     const arr = str.split('');
+//     return arr.reverse().join('');
+// }
+
+// Loop over the string
+function reverse(str) {
+    let reverse = '';
+    for (let char of str) {
+        reverse = char + reverse;
+    }
+    return reverse;
+}
 
 module.exports = reverse;
