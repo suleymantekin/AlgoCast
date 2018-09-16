e93116a42fb441a7be382ca99a49f2743e68cb63
suleymantekin <suleymantekin@users.noreply.github.com>
2018-09-16 18:34:45 +0300

    Return most commonly used character using a Map

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,19 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+    const map = new Map();
+    for (let char of str) {
+        map.set(char, map.get(char) + 1 || 1);
+    }
+
+    let max = str[0];
+    for (let [key, val] of map) {
+        if (val > map.get(max)) {
+            max = key;
+        }
+    }
+    return max;
+}
 
 module.exports = maxChar;
