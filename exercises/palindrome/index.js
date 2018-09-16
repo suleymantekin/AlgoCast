29d46e9f523d9a1cff01f941bf5dd6335bacd30f
suleymantekin <suleymantekin@users.noreply.github.com>
2018-09-16 09:46:57 +0300

    My solution for Palindrome

--- exercises/palindrome/index.js
+++ exercises/palindrome/index.js
@@ -7,6 +7,13 @@
 //   palindrome("abba") === true
 //   palindrome("abcdefg") === false
 
-function palindrome(str) {}
+function palindrome(str) {
+    for (let i = 0; i < Math.floor(str.length / 2); i++) {
+        if (str[i] !== str[str.length - i - 1]) {
+            return false
+        }
+    }
+    return true;
+}
 
 module.exports = palindrome;
