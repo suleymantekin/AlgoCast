ce16fb562177b26911b7f993e1b7b95021452f17
suleymantekin <suleymantekin@users.noreply.github.com>
2018-09-16 10:15:12 +0300

    My solution for ReverseInt

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,17 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+function reverseInt(n) {
+    const flag = n < 0;
+    if (flag) {
+        n = Math.abs(n)
+    }
+    let res = 0;
+    while (n > 0) {
+        res = res * 10 + (n % 10);
+        n = Math.floor(n / 10);
+    }
+    return flag ? res * (-1) : res;
+}
 
 module.exports = reverseInt;
