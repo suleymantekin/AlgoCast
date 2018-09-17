ae96f30acc7fa947e5b7ef90941d85e8ff1338c3
suleymantekin <slymntkn34@gmail.com>
2018-09-17 22:12:03 +0300

    Log numbers according to fizzbuzz problem

--- exercises/fizzbuzz/index.js
+++ exercises/fizzbuzz/index.js
@@ -12,6 +12,17 @@
 //   4
 //   buzz
 
-function fizzBuzz(n) {}
+function fizzBuzz(n) {
+    for (let i = 1; i <= n; i++) {
+        let text = '';
+        if (i % 3 == 0) {
+            text += 'fizz';
+        }
+        if (i % 5 == 0) {
+            text += 'buzz';
+        }
+        console.log(text ? text : i)
+    }
+}
 
 module.exports = fizzBuzz;
