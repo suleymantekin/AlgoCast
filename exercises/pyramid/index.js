eb362a8e1061c3c2aa86f72e51124411cce83032
suleymantekin <slymntkn34@gmail.com>
2018-09-18 23:17:35 +0300

    Log Pyramid shape recursive solution

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -14,20 +14,43 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n) {
-    const numOfColumn = 2 * n - 1;
-    const center = Math.floor(numOfColumn / 2)
-    for (let row = 0; row < n; row++) {
-        let level = '';
-        for (let column = 0; column < numOfColumn; column++) {
-            if (column >= center - row && column <= center + row) {
-                level += '#'
-            } else {
-                level += ' '
-            }
-        }
-        console.log(level)
+// My solution and first solution
+// function pyramid(n) {
+//     const numOfColumn = 2 * n - 1;
+//     const center = Math.floor(numOfColumn / 2)
+//     for (let row = 0; row < n; row++) {
+//         let level = '';
+//         for (let column = 0; column < numOfColumn; column++) {
+//             if (column >= center - row && column <= center + row) {
+//                 level += '#'
+//             } else {
+//                 level += ' '
+//             }
+//         }
+//         console.log(level)
+//     }
+// }
+
+// Recursive solution
+function pyramid(n, row = 0, level = '') {
+    if (n === row) {
+        return;
     }
+
+    const numOfColumn = 2 * n - 1;
+    if (numOfColumn === level.length) {
+        console.log(level);
+        return pyramid(n, row + 1);
+    }
+
+    const center = Math.floor(numOfColumn / 2)
+    if (level.length >= center - row && level.length <= center + row) {
+        level += '#';
+    } else {
+        level += ' ';
+    }
+
+    pyramid(n, row, level)
 }
 
 module.exports = pyramid;
