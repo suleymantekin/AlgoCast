16b89dc9a0cd046162068427107107217bad98b6
suleymantekin <slymntkn34@gmail.com>
2018-09-19 23:40:22 +0300

    Count the vowels my solution

--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -7,6 +7,15 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+function vowels(str) {
+    const vowels = 'aeiou';
+    let count = 0;
+    for (let char of str.toLowerCase()) {
+        if (vowels.indexOf(char) != -1) {
+            count++;
+        }
+    }
+    return count;
+}
 
 module.exports = vowels;
