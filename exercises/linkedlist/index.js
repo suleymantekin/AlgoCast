93aa984ebf33e2949e40bf1e78fc86b97e7a5705
suleymantekin <slymntkn34@gmail.com>
2018-09-26 07:54:34 +0300

    Implement for..of loop for LinkedList

--- exercises/linkedlist/index.js
+++ exercises/linkedlist/index.js
@@ -153,6 +153,14 @@ class LinkedList {
             node = node.next;
         }
     }
+
+    *[Symbol.iterator]() {
+        let node = this.head;
+        while (node) {
+            yield node;
+            node = node.next;
+        }
+    }
 }
 
 module.exports = { Node, LinkedList };
