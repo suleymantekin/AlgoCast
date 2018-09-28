91a00943e550c6610b3c183fdaccb523136d56ef
suleymantekin <slymntkn34@gmail.com>
2018-09-28 21:47:20 +0300

    Find the width of each tree level

--- exercises/levelwidth/index.js
+++ exercises/levelwidth/index.js
@@ -11,6 +11,21 @@
 // 4       5
 // Answer: [1, 3, 2]
 
-function levelWidth(root) {}
+function levelWidth(root) {
+    let children = [root, 's'];
+    let result = [0];
+    while (children.length > 1) {
+        let element = children.shift();
+        if (element === 's') {
+            result.push(0);
+            children.push('s');
+        } else {
+            children.push(...element.children);
+            result[result.length - 1]++;
+        }
+    }
+
+    return result;
+}
 
 module.exports = levelWidth;
